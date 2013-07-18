66f43d459c3dd2b127c60cfcaf19595f719f2eb0
Yan Rabovik <yan@rabovik.ru>
2013-07-18 15:56:39 +0400

    Project template

---
+++ RSDeallocHandlerTests/RSDeallocHandlerTests/RSDeallocHandlerTests.h
@@ -0,0 +1,13 @@
+//
+//  RSDeallocHandlerTests.h
+//  RSDeallocHandlerTests
+//
+//  Created by Yan Rabovik on 18.07.13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RSDeallocHandlerTests : SenTestCase
+
+@end
---
+++ RSDeallocHandlerTests/RSDeallocHandlerTests/RSDeallocHandlerTests.m
@@ -0,0 +1,32 @@
+//
+//  RSDeallocHandlerTests.m
+//  RSDeallocHandlerTests
+//
+//  Created by Yan Rabovik on 18.07.13.
+//
+//
+
+#import "RSDeallocHandlerTests.h"
+
+@implementation RSDeallocHandlerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RSDeallocHandlerTests");
+}
+
+@end
