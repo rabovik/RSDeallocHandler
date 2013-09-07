3aeffffc4b4054942ebb29d9aeb9a0cd435c1b75
Yan Rabovik <yan@rabovik.ru>
2013-09-07 14:37:25 +0400

    Documentation comments.

--- RSDeallocHandler/NSObject+RSDeallocHandler.h
+++ RSDeallocHandler/NSObject+RSDeallocHandler.h
@@ -7,15 +7,31 @@
 
 @interface NSObject (RSDeallocHandler)
 
+/// ----------------------------------------
+#pragma mark - Adding Handlers
+/// @name      Adding Handlers
+/// ----------------------------------------
+
 /**
- *  @param owner If owner is not nil, then handler will be automatically
- *  removed from the receiver and deallocated when the owner object dies
- *  @return uniqie ID that can be used to remove handler
+ Adds dealloc handler to the receiver.
+ 
+ @param handler The block object to be executed upon the `-dealloc` method of the receiver.
+ @param owner If owner is not nil, then handler will be automatically removed from the receiver and deallocated when the owner object dies.
+ @return Uniqie ID that can be used to remove the handler.
+ @see -rs_removeDeallocHandler:
  */
 -(NSString *)rs_addDeallocHandler:(dispatch_block_t)handler owner:(id)owner;
 
+/// ----------------------------------------
+#pragma mark - Removing Handlers
+/// @name      Removing Handlers
+/// ----------------------------------------
+
 /**
- *  @param uid Unique ID of the handler to remove
+ Removes the dealloc handler from the receiver.
+ 
+ @param uid Unique ID of the handler to be removed.
+ @see -rs_addDeallocHandler:owner:
  */
 -(void)rs_removeDeallocHandler:(NSString *)uid;
 
